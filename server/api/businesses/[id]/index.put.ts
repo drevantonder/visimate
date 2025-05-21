426cac387335232446418304d8913c9efdc11dde
Andre van Tonder <andre@drevan.me>
2025-05-22 09:07:51 +1000

    refactor: More tidying up

--- server/api/businesses/[id]/index.put.ts
+++ server/api/businesses/[id]/index.put.ts
@@ -1,15 +1,7 @@
 import { createUpdateSchema } from 'drizzle-zod';
 
 export default defineEventHandler(async (event) => {
-  const id = event.context.params?.id;
-  if (!id) {
-    throw createError({ statusCode: 400, statusMessage: 'Missing business ID' });
-  }
-  const businessId = parseInt(id, 10);
-  if (isNaN(businessId)) {
-    throw createError({ statusCode: 400, statusMessage: 'Invalid business ID' });
-  }
-
+  const { id } = await getValidatedRouterParams(event, z.object({ id: z.coerce.number() }).parse);
   const body = await readValidatedBody(event, createUpdateSchema(tables.businesses).parse);
 
   if (Object.keys(body).length === 0) {
@@ -21,7 +13,7 @@ export default defineEventHandler(async (event) => {
     const result = await db
       .update(tables.businesses)
       .set({ ...body, updatedAt: new Date().toISOString() }) // Ensure updatedAt is updated
-      .where(eq(tables.businesses.id, businessId))
+      .where(eq(tables.businesses.id, id))
       .returning();
 
     if (result.length === 0) {
@@ -30,7 +22,6 @@ export default defineEventHandler(async (event) => {
 
     return result[0];
   } catch (error) {
-    // Handle potential database errors or other issues
     console.error('Error updating business:', error);
     throw createError({ statusCode: 500, statusMessage: 'Failed to update business' });
   }
